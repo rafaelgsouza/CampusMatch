80d561af49be0c1d8e203c5f3a781eb989344e01
Diego A dos Santos <diegoaugusto001@gmail.com>
2024-12-13 10:06:43 -0300

    Criando o repository  da tabela instituicao_publicação

---
+++ campus_match_spring/src/main/java/br/edu/ifsp/campus_match_spring/repository/InstituicaoPublicacaoRepo.java
@@ -0,0 +1,8 @@
+package br.edu.ifsp.campus_match_spring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.edu.ifsp.campus_match_spring.model.InstituicaoPublicacao;
+
+public interface InstituicaoPublicacaoRepo extends JpaRepository<InstituicaoPublicacao, Long> {
+
+}
